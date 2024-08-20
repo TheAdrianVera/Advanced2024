5f6e27ece54c46ccdcb9e081f3cc6521a9e52ced
TheAdrianVera <agvm94@gmail.com>
2024-08-20 10:38:37 -0500

    added advanced colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header () {
     return (
-        <header className="sticky top-0 bg-gray-800 text-white h-20 flex items-center">
+        <header className="sticky top-0 bg-advancedDarkBlue text-white h-20 flex items-center">
         </header>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        advancedNavyBlue: '#003d68',
+        advancedLightBlue: '#5fa3d9',
+        advancedDarkBlue: '#002947',
+        advancedGrey: '#c9cacc',
+        advancedBlue: '#4f97e8',
+        advancedDarkGrey: '#6b6b6b',
+        advancedMutedWhite: '#dee5f2',
+      }
+    },
   },
   plugins: [],
 }
