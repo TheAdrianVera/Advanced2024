8cf3c9df1336eb83182f70bc493c6e9d715a573c
TheAdrianVera <agvm94@gmail.com>
2024-08-20 23:52:15 -0500

    updated header with logo

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { TiThMenu } from "react-icons/ti"
+import Logo from './Logo'
 
 interface MobileNavProps {
     className?: string
@@ -7,8 +8,9 @@ interface MobileNavProps {
 
 const MobileNav: React.FC<MobileNavProps> = ({ className }) => {
     return (
-        <div className={`${className} flex flex-row items-center px-5`}>
-            <TiThMenu className='hover:text-advancedLightBlue' size={32}/> 
+        <div className={`${className} flex flex-row items-center justify-between px-5 w-[100%]`}>
+            <Logo size={40} className='ml-2'/>
+            <TiThMenu className='hover:text-advancedLightBlue' size={32}/>
         </div>
     )
 }
