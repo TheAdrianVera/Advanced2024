cf3f1820ed537193e032bbcc72567216693f088f
TheAdrianVera <agvm94@gmail.com>
2024-08-27 14:23:52 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Careers from './pages/Careers'
@@ -11,17 +12,20 @@ import Contact from './pages/Contact'
 function App() {
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/careers' element={<Careers />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/community' element={<Community />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-    </>
+      <main className="flex-grow">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/careers' element={<Careers />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/community' element={<Community />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
