93260e262afcd37bf5022f265d8df0f9d2fb6038
TheAdrianVera <agvm94@gmail.com>
2024-08-27 21:07:39 -0500

    fixed map spacing and styling

--- src/components/pagesections/ContactSection.tsx
+++ src/components/pagesections/ContactSection.tsx
@@ -20,7 +20,7 @@ const ContactSection: React.FC = () => {
                     <h1 className='section-title'>Contact Us</h1>
                 </div>
                 <div className='flex flex-col md:flex-row py-10'>
-                    <div className='contact-map md:pr-10 w-100vw h-50vh md:w=50vw'>
+                    <div className='contact-map w-100vw h-50vh md:pr-10 md:w-50vw md:h-25vh'>
                         <APIProvider apiKey={apiKey}>
                             <Map
                                 className='map-container'
@@ -32,11 +32,17 @@ const ContactSection: React.FC = () => {
                         </APIProvider>
                     </div>
                     <div className='contact-info'>
-                        <h2>Our Location</h2>
-                        <p>123 Main St. <br /> Anytown, USA 12345</p>
-                        <h2>Phone</h2>
-                        <p>(123) 456-7890</p>
+                        <h2>Advanced Healthcare Services LLC.</h2>
+                        <p>3900 Pintail Drive, Suite A <br /> Springfield, IL 62711</p>
+                        <h2>Phone:</h2>
+                        <p>(217) 726-6956</p>
+                        <h2>Fax</h2>
+                        <p>(217) 726-7082</p>
                         <h2>Email</h2>
+                        <p>info@ahsllc.org</p>
+                        <h2>Hours</h2>
+                        <p>Monday - Friday: 8:00 AM to 4:30 PM</p>
+                        <p>Saturday - Sunday: On Call</p>
                     </div>
                 </div>
 
