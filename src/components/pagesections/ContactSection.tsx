5877e1e92732116a4945444cffa5151bd3600735
TheAdrianVera <agvm94@gmail.com>
2024-08-27 20:59:06 -0500

    updated styling of map section

--- src/components/pagesections/ContactSection.tsx
+++ src/components/pagesections/ContactSection.tsx
@@ -15,26 +15,31 @@ const ContactSection: React.FC = () => {
 
     return (
         <div className='section'>
-            <h1>Contact Us</h1>
-            <div className='flex flex-row'>
-                <div className='contact-map'>
-                    <APIProvider apiKey={apiKey}>
-                        <Map
-                            style={{width: '50vw', height: '50vh'}}
-                            defaultCenter={{lat: 22.54992, lng: 0}}
-                            defaultZoom={3}
-                            gestureHandling={'greedy'}
-                            disableDefaultUI={true}
-                        />
-                    </APIProvider>
+            <div className='flex flex-col'>
+                <div className='flex justify-center py-10'>
+                    <h1 className='section-title'>Contact Us</h1>
                 </div>
-                <div className='contact-info'>
-                    <h2>Our Location</h2>
-                    <p>123 Main St. <br /> Anytown, USA 12345</p>
-                    <h2>Phone</h2>
-                    <p>(123) 456-7890</p>
-                    <h2>Email</h2>
+                <div className='flex flex-col md:flex-row py-10'>
+                    <div className='contact-map md:pr-10 w-100vw h-50vh md:w=50vw'>
+                        <APIProvider apiKey={apiKey}>
+                            <Map
+                                className='map-container'
+                                defaultCenter={{lat: 22.54992, lng: 0}}
+                                defaultZoom={3}
+                                gestureHandling={'greedy'}
+                                disableDefaultUI={true}
+                            />
+                        </APIProvider>
+                    </div>
+                    <div className='contact-info'>
+                        <h2>Our Location</h2>
+                        <p>123 Main St. <br /> Anytown, USA 12345</p>
+                        <h2>Phone</h2>
+                        <p>(123) 456-7890</p>
+                        <h2>Email</h2>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
