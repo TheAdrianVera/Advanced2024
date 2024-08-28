3bf8df3bbcf5786ef8c4b12838e7eb14b582dd53
TheAdrianVera <agvm94@gmail.com>
2024-08-27 19:58:23 -0500

    added google maps component to contact section

--- src/components/pagesections/ContactSection.tsx
+++ src/components/pagesections/ContactSection.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
 
 interface ContactSectionProps {
 
 }
 
+const containerStyle = {
+    width: '800px',
+    height: '400px'
+}
+
 const ContactSection: React.FC = () => {
+    const apiKey = 'AIzaSyAZXGtGLFPg67JJm4IyKqQjx8Plgdx4whI';
+
     return (
         <div className='section'>
             <h1>Contact Us</h1>
             <div className='flex flex-row'>
                 <div className='contact-map'>
-
+                    <APIProvider apiKey={apiKey}>
+                        <Map
+                            style={{width: '50vw', height: '50vh'}}
+                            defaultCenter={{lat: 22.54992, lng: 0}}
+                            defaultZoom={3}
+                            gestureHandling={'greedy'}
+                            disableDefaultUI={true}
+                        />
+                    </APIProvider>
                 </div>
                 <div className='contact-info'>
                     <h2>Our Location</h2>
