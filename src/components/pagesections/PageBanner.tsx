2e9e2337cb490b73e621ed04c4193c08fa122249
TheAdrianVera <agvm94@gmail.com>
2024-08-28 21:30:29 -0500

    updated page banner functionality, accepts image banners

--- src/components/pagesections/PageBanner.tsx
+++ src/components/pagesections/PageBanner.tsx
@@ -7,21 +7,32 @@ interface PageBannerProps {
         text: string
         link: string
     }
+    backgroundUrl?: string
 }
 
-const PageBanner: React.FC<PageBannerProps> = ({ title, subtitle, button }) => {
+const PageBanner: React.FC<PageBannerProps> = ({ title, subtitle, button, backgroundUrl }) => {
+    const backgroundStyle = backgroundUrl ? { backgroundImage: `url(${backgroundUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' } : {};
 
     return (
-        <div className="bg-advancedBlue text-white py-8 px-4 h-[15rem] flex flex-col items-center justify-center">
-            <div>
+        <div
+            className={`relative text-white py-8 px-4 h-[15rem] flex flex-col items-center justify-center ${backgroundUrl ? '' : 'bg-advancedBlue'}`}
+            style={backgroundStyle}
+        >
+            {backgroundUrl && (
+                <div className="absolute inset-0 bg-advancedLightBlue opacity-50"></div> // Change this line for different overlay colors and opacities
+            )}
+
+            <div className="relative z-10">
                 <h1 className="text-4xl font-bold mb-2 uppercase">{title}</h1>
                 {subtitle && (
-                     <p className="text-lg">{subtitle}</p>
+                    <p className="text-lg">{subtitle}</p>
                 )}
             </div>
+
             {button && (
                 <a href={button.link} className="bg-advancedNavyBlue text-white px-4 py-2 mt-4 rounded-md hover:bg-advancedLightBlue">{button.text}</a>
             )}
+            
         </div>
     )
 }
