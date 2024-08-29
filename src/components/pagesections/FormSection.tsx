019b2ef5aa3cd4db090468d289b2be35b9e0807c
TheAdrianVera <agvm94@gmail.com>
2024-08-29 09:25:09 -0500

    added form section template

---
+++ src/components/pagesections/FormSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// interface FormSectionProps {}
+
+const FormSection:React.FC = () => {
+    return (
+        <div className='section bg-white'>
+            <div className='flex flex-col'>
+                <div className='flex justify-center py-10'>
+                    <h1 className='section-title'>Referral Form</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormSection
