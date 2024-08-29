e403b3d202770b1813095418539c1a67aa05d21b
TheAdrianVera <agvm94@gmail.com>
2024-08-28 20:48:52 -0500

    added map to contact page

--- src/components/pagesections/ContactSection.tsx
+++ src/components/pagesections/ContactSection.tsx
@@ -1,53 +1,9 @@
 import React from 'react'
-import { 
-    Marker,
-    APIProvider, 
-    Map
-} from '@vis.gl/react-google-maps'
 
 interface ContactSectionProps {}
 
-type Poi ={ 
-    key: string, 
-    location: google.maps.LatLngLiteral, 
-    label?: string,
-    labelColor?: string
-}
-
-const containerStyle = {
-    width: '800px',
-    height: '400px'
-}
-
-const locations: Poi[] = [
-    {
-        key: "Advanced Healthcare LLC.", 
-        location: {lat: 39.74320052102959, lng: -89.71238270299604},
-        label: `Advanced \nHealthcare LLC.`,
-        labelColor: "#FF0000",
-    }
-]
-
-const PoiMarkers = (props: {pois: Poi[]}) => {
-    return (
-        <>
-            {props.pois.map((poi: Poi) => (
-                <Marker
-                    key={poi.key}
-                    position={poi.location}
-                    label={{ 
-                        text: poi.label || '', 
-                        color: poi.labelColor,
-                        className: 'marker-label'
-                    }}
-                />
-            ))}
-        </>
-    )
-}
-
 const ContactSection: React.FC = () => {
-    const apiKey = 'AIzaSyAZXGtGLFPg67JJm4IyKqQjx8Plgdx4whI'
+    const apiKey = import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''
 
     return (
         <div className='section'>
@@ -56,17 +12,16 @@ const ContactSection: React.FC = () => {
                     <h1 className='section-title'>Contact Us</h1>
                 </div>
                 <div className='flex flex-col md:flex-row py-10'>
-                    <div className='contact-map w-100vw h-50vh md:pr-10 md:w-50vw md:h-25vh'>
-                        <APIProvider apiKey={apiKey}>
-                            <Map
-                                className='map-container'
-                                defaultZoom={15}
-                                defaultCenter={ { lat: 39.74320052102959, lng: -89.71238270299604 } }
-                            >
-                                <PoiMarkers pois={locations} />
-                            </Map>
-                        </APIProvider>
-                    </div>
+                        <iframe
+                            
+                            height="400px"
+                            loading="lazy"
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAZXGtGLFPg67JJm4IyKqQjx8Plgdx4whI
+                                &q=Advanced+Healthcare+Services+Springfield"
+                            className='w-full md:w-1/2 md:mr-5'
+                        >
+                        </iframe>
+
                     <div className='contact-info mt-10 md:mt-0'>
                         <h2 className='mb-1 font-bold'>Advanced Healthcare Services LLC.</h2>
                         <p className='pb-3'>3900 Pintail Drive, Suite A <br /> Springfield, IL 62711</p>
