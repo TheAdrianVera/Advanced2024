28c36a71bba632bdfbd457cf8b5ea5df8289166d
TheAdrianVera <agvm94@gmail.com>
2024-09-06 23:42:17 -0500

    added webdev link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-advancedDarkBlue text-white py-4">
             <div className="container mx-auto text-center">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Advanced Healthcare LLC. All rights reserved.</p>
+                <p className="text-sm">&copy; {new Date().getFullYear()} Advanced Healthcare LLC. All rights reserved. Web Dev: adriangvera.com</p>
             </div>
         </footer>
     )
