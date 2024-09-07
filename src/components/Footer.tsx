485eaa58f455cc9fe10d706700ab73a807edfbab
TheAdrianVera <agvm94@gmail.com>
2024-09-06 23:58:58 -0500

    updated footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-advancedDarkBlue text-white py-4">
             <div className="container mx-auto text-center">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Advanced Healthcare LLC. All rights reserved. Web Dev: adriangvera.com</p>
+                <p className="text-sm">&copy; {new Date().getFullYear()} Advanced Healthcare LLC. All rights reserved. Web Dev: <a href="https://www.adriangvera.com" target="_blank" className="text-white hover:text-blue-300">adriangvera.com</a></p>
             </div>
         </footer>
     )
