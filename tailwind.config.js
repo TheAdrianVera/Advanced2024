49a65fada2b82ddaf45ca5f9ce44380146e349d6
TheAdrianVera <agvm94@gmail.com>
2024-09-10 20:17:17 -0500

    updated tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        arial: ['Arial', 'sans-serif'],
+        'arial-black': ['Arial Black', 'sans-serif'],
+        'arial-bold': ['Arial Bold', 'sans-serif'],
+        'arial-bold-italic': ['Arial Bold Italic', 'sans-serif'],
+        'arial-italic': ['Arial Italic', 'sans-serif'],
+        'arial-narrow': ['Arial Narrow', 'sans-serif'],
+        'arial-narrow-bold': ['Arial Narrow Bold', 'sans-serif'],
+        'arial-narrow-bold-italic': ['Arial Narrow Bold Italic', 'sans-serif'],
+        'arial-narrow-italic': ['Arial Narrow Italic', 'sans-serif'],
+        'arial-rounded': ['Arial Rounded MT Bold', 'sans-serif'],
+        'arial-rounded-bold': ['Arial Rounded MT Bold', 'sans-serif'],
+        'arial-unicode': ['Arial Unicode MS', 'sans-serif'],
+        'avenir': ['Avenir', 'sans-serif'],
+
+      },
       colors: {
         advancedNavyBlue: '#003d68',
         advancedLightBlue: '#5fa3d9',
