b352443991dcff25f441e7c41a53c3df87347153
TheAdrianVera <agvm94@gmail.com>
2024-09-10 22:16:00 -0500

    added sections to service page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PageBanner from '../components/pagesections/PageBanner'
+import ServicesSection from '../components/pagesections/ServicesSection'
+import ContactSection from '../components/pagesections/ContactSection'
 
 const Services: React.FC = () => {
   return (
@@ -9,10 +11,8 @@ const Services: React.FC = () => {
         subtitle='Learn more about the many Healthcare Services we provide.'
         backgroundUrl='https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/header-bg.jpg'
       />
-      <div>
-        <h1>Service Page</h1>
-        <p>Service Secret</p>
-      </div>
+      <ServicesSection />
+      <ContactSection />
     </>
 
   )
