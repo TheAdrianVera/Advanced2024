46027ef5318f7ef426064c8bbd99d2a8b2abce09
TheAdrianVera <agvm94@gmail.com>
2024-09-10 22:13:40 -0500

    updated servicesSection component

--- src/components/pagesections/ServicesSection.tsx
+++ src/components/pagesections/ServicesSection.tsx
@@ -11,48 +11,48 @@ const ServicesSection: React.FC = () => {
                 </div>
             </div>
             <div className='flex flex-col'>
-                <div className='flex flex-col md:flex-row'>
+                <div className='flex flex-col lg:flex-row pb-5'>
                     <ServiceBlock 
                         title='Skilled Nursing' 
                         content={`Advanced Healthcare Services, LLC nurses oversee patient progress, and assist the patients and caregivers to understand: \nStages of illness and recovery \nTreatments, diet, and medication, \nSymptoms in Recurring Illness`}
-                        icon='icon1' 
+                        iconLabel='iconLabel1' 
                     />
                     <ServiceBlock 
                         title='Physical Therapy'
                         content='Advanced Healthcare Services, LLC evaluates and develops a plan of treatment and establishes home programs to improve mobility, transfer skills, ambulation, gait, balance and coordination.'
-                        icon='icon2'
+                        iconLabel='iconLabel2'
                     />
                     <ServiceBlock 
                         title='Home Health Aids'
                         content='Advanced Healthcare Services, LLC employs state certified nurse assistants who are trained to assist patients with their personal care, hygiene, grooming, and activities of daily living.'
-                        icon='icon3'
+                        iconLabel='iconLabel3'
                     />
                     <ServiceBlock 
                         title='Medical Equipment'
                         content='Advanced Healthcare Services, LLC can arrange for rental and/or purchase of wheelchairs, walkers, special beds, or any assistive devices which may be needed.'
-                        icon='icon4'           
+                        iconLabel='iconLabel4'           
                     />
                 </div>
-                <div className='flex flex-col md:flex-row'>
+                <div className='flex flex-col lg:flex-row pb-5'>
                     <ServiceBlock 
                         title='Medical Social Worker'
                         content='Advanced Healthcare Services, LLC provides social workers that can provide knowledge and information about community resources. As well as social and emotional support for patients and caregivers.'
-                        icon='icon5'
+                        iconLabel='iconLabel5'
                     />
                     <ServiceBlock 
                         title='Occupational Therapy'
                         content='Advanced Healthcare Services, LLC evaluates and provide a plan of care in the use of therapeutic , creative self-care activities of daily living and adaption to their living environment.'
-                        icon='icon6'
+                        iconLabel='iconLabel6'
                     />
                     <ServiceBlock 
                         title='Speech Therapy'
                         content={`Advanced Healthcare Services, LLC will evaluate and provide treatment for patients to improve their communication skills. Speech Therapists will also evaluate the patient's ability to swallow safe.`}
-                        icon='icon7'
+                        iconLabel='iconLabel7'
                     />
                     <ServiceBlock 
                         title='Other Services'
                         content='Advanced Healthcare Services, LLC upon your request, can coordinate other services such as finding homemakers, companions or sitters.'
-                        icon='icon8'
+                        iconLabel='iconLabel8'
                     />
                 </div>
             </div>
