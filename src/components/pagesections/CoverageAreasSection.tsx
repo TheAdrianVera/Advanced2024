779e47bcb85d9742f352467a2a2caab03c540c9b
TheAdrianVera <agvm94@gmail.com>
2024-09-10 20:22:01 -0500

    added proper Title and subheading to sections

--- src/components/pagesections/CoverageAreasSection.tsx
+++ src/components/pagesections/CoverageAreasSection.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const CoverageAreasSection: React.FC = () => {
     return (
-        <div>
-            <h1>Coverage Areas Section</h1>
+        <div className='section px-5 md:px-20'>
+            <div className='flex flex-col items-center py-10'>
+                <div className='flex flex-col items-center'>
+                    <h1 className='section-title mb-10'>Coverage Areas</h1>
+                    <h2 className='text-xl font-arial-narrow md:text-2xl text-gray-800 mb-6'>Illinois Counties We Cover</h2>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pagesections/ServicesSection.tsx
+++ src/components/pagesections/ServicesSection.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ServicesSection: React.FC = () => {
     return (
-        <div>
-            <h1>Services Section</h1>
+        <div className='section px-5 md:px-20'>
+            <div className='flex flex-col items-center py-10'>
+                <div className='flex flex-col items-center'>
+                    <h1 className='section-title mb-4'>Services</h1>
+                    <h2 className='text-xl font-arial-narrow md:text-2xl text-gray-800 mb-6'>A Look Into the Many Healthcare Services We Provide</h2>
+                </div>
+            </div>
         </div>
     )
 }
