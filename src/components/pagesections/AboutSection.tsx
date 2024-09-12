0a0050ae78f790fd3a7ef26905ea81faba794cdc
TheAdrianVera <agvm94@gmail.com>
2024-09-12 16:59:43 -0500

    added about section metadata

---
+++ src/components/pagesections/AboutSection.tsx
@@ -0,0 +1,33 @@
+
+const aboutData = {
+    section1Title: 'Who We Are',
+    section1Para1: 'At Advanced Healthcare Services, LLC, we are focused on providing home health services with the highest levels of patient satisfaction.',
+    section1Para2: 'We will do everything we can to meet your expectations. With a variety of services - from Skilled Nursing to Podiatry - we’re sure you’ll be happy working with us.',
+    section2Title: 'Home Health Care',
+    section2Para1: 'Home health care allows patients to maintain quality of life and receive personlized health care in the comfort of their home.',
+    section2Para2: 'In addition ot being a cost-effective option for receiving health care services, home health promotes quicker recover and improves the quality of life for both patient and their family.',
+    section3Title: 'Criteria For Homebound Status',
+    section3SubTitle: `The following criteria are considered when determining homebound status. To be considered homebound, both criteria one and two must be met.`,
+    section3Criteria: [
+        {
+            content1: `Beneficiary's medical condition restricts the ability to leave home without the assistance of another individual or without the assistance of a supportive device.`,
+            content2: `The beneficiary's medical condition should restrict the ability to leave home.`,
+            content3: 'If, however, the the beneficiary chooses to leave home frequently, even though leaving home requires a considerable and taxing effort, the beneficiary would not be considered homebound.'
+        },
+        {
+            content1: 'Beneficiary leaves home only to receive medical treatment that generally cannot be provided in the home.',
+            content2: 'Leaves the home infrequently; for short periods for non-medical purposes or to attend a religious service or unique event such as a funeral or graduation, and these absences do not indicate the beneficiary has the ability to obtain health care provided outside the home, then the beneficiary may still be considered homebound.',
+            content3: `To determine whether the beneficiary has the general ability to leave the home, look at the beneficiary's condition over a period of a time rather than short periods within the home health stay.`
+        }
+    ],
+    videoUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/advanced_home_video.mp4',
+    videoPoster: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/home-video-ph.png',
+}
+
+const AboutSection = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default AboutSection
