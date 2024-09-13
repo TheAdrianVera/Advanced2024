59c5259b380f1284139b8f8ec231a3938805706d
TheAdrianVera <agvm94@gmail.com>
2024-09-13 18:38:40 -0500

    added meta data to appropriate sections

--- src/components/pagesections/CommunityEventsSection.tsx
+++ src/components/pagesections/CommunityEventsSection.tsx
@@ -1,3 +1,22 @@
+const easterEggHuntGallery = [
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggy.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Posing for a Picture',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggz.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Posing for a Picture and Cheering',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggx.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Examinining their Egg Bags',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggw.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Showing off their Egg Bags',
+    }
+]
+
 const CommunityEventsSection:React.FC = () => {
     return (
         <div>
--- src/components/pagesections/CommunitySection.tsx
+++ src/components/pagesections/CommunitySection.tsx
@@ -1,3 +1,66 @@
+const communityGallery = [
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career8.png',
+        alt: 'Advanced Team Members Posing for a Picture',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community2.jpeg',
+        alt: 'Advanced Patient Petting Advanced Dog',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community3.jpeg',
+        alt: 'Advanced Healthcare Mobile (side view)',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career9.jpg',
+        alt: 'Advanced Team Members Posing in front of Advanced Healthcare Poster',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community1.jpeg',
+        alt: 'Advanced Healthcare Community Members Posing in Fall Attire',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/fb_ad_photo.JPG',
+        alt: 'Advanced Healthcare Team Member taking Vidalsigns of a Patient',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community6.jpeg',
+        alt: 'Advanced Team Members Posing at a Advanced Healthcare Table',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career3.jpg',
+        alt: 'Advanced Team Member Posing with Advanced Dog',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/careers7.jpg',
+        alt: 'Advanced Team Members Posing in front of Office',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community5.jpeg',
+        alt: 'Advanced Team Member Posing in front of Advanced Logo',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career1.jpg',
+        alt: 'Advanced Team Posing with Medical Hats on',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community4.jpeg',
+        alt: 'Advanced Team at Advanced Healthcare Table in Pirate Attire for Halloween',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career2.jpg',
+        alt: 'Advanced Team Member Posing at Event',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career4.jpg',
+        alt: 'Advanced Holiday Greeting Card',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community8.jpg',
+        alt: 'Advanced Team Members having a Meeting',
+    }
+]
+
 const CommunitySection: React.FC = () => {
     return (
         <div>
