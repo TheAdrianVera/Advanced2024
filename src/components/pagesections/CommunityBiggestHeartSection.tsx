59bc5fa0773d03b50475f9cc584ae4cf4ab7b2fa
TheAdrianVera <agvm94@gmail.com>
2024-09-13 18:37:24 -0500

    added Biggest Heart Award data

--- src/components/pagesections/CommunityBiggestHeartSection.tsx
+++ src/components/pagesections/CommunityBiggestHeartSection.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { IoMdGitPullRequest } from "react-icons/io"
 
 const biggestHeartAwards = [
     {
@@ -9,6 +10,7 @@ const biggestHeartAwards = [
         content2: 'The impact they have on our community unequivocally has changed countless lives. Because of the dedicated staff and countless accessible programs, we have witnessed individuals in our community thrive while maintaining their independence.',
         link: 'https://www.facebook.com/share/p/Ma2FN7xroYzpkeWx/',
         imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/bh-kelley.png',
+        imgAlt: 'Biggest Heart Award Winners Posing and Smiling',
 
     },
     {
@@ -19,6 +21,7 @@ const biggestHeartAwards = [
         content2: 'Misty has gone above and beyond in being an advocate for Blind and Visually impared residents. Thank you for everything you do for the residents!',
         link: 'https://www.facebook.com/share/p/ifvkevcEEcUcjUAf/',
         imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/mistyduncan.png',
+        imgAlt: 'Biggest Heart Award Winners Posing and Smiling with their Award',
     },
     {
         name: 'Kelley Tireny',
@@ -28,8 +31,93 @@ const biggestHeartAwards = [
         content2: 'Kelley cares deeply, loves big and spreads joy and compassion to all that meet her. Thank you Kelley King Tierne from Carriage Crossing Rochester for so generously impacting the lives of our seniors.',
         link: 'https://www.facebook.com/share/p/YWk43Wt5W7pa8n8k/',
         imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/biggest_heart.png',
+        imgAlt: 'Biggest Heart Award Winners Posing and Presenting their Award',
     },
 ]
+
+const easterEggHuntGallery = [
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggy.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Posing for a Picture',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggz.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Posing for a Picture and Cheering',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggx.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Examinining their Egg Bags',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/eastereggw.png',
+        alt: 'Advanced Healthcare Easter Egg Hunt Participants Showing off their Egg Bags',
+    }
+]
+
+const communityGallery = [
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career8.png',
+        alt: 'Advanced Team Members Posing for a Picture',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community2.jpeg',
+        alt: 'Advanced Patient Petting Advanced Dog',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community3.jpeg',
+        alt: 'Advanced Healthcare Mobile (side view)',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career9.jpg',
+        alt: 'Advanced Team Members Posing in front of Advanced Healthcare Poster',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community1.jpeg',
+        alt: 'Advanced Healthcare Community Members Posing in Fall Attire',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/fb_ad_photo.JPG',
+        alt: 'Advanced Healthcare Team Member taking Vidalsigns of a Patient',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community6.jpeg',
+        alt: 'Advanced Team Members Posing at a Advanced Healthcare Table',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career3.jpg',
+        alt: 'Advanced Team Member Posing with Advanced Dog',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/careers7.jpg',
+        alt: 'Advanced Team Members Posing in front of Office',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community5.jpeg',
+        alt: 'Advanced Team Member Posing in front of Advanced Logo',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career1.jpg',
+        alt: 'Advanced Team Posing with Medical Hats on',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community4.jpeg',
+        alt: 'Advanced Team at Advanced Healthcare Table in Pirate Attire for Halloween',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career2.jpg',
+        alt: 'Advanced Team Member Posing at Event',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/career4.jpg',
+        alt: 'Advanced Holiday Greeting Card',
+    },
+    {
+        imgUrl: 'https://advanced-healthcare-llc-web-images.s3.us-east-2.amazonaws.com/career/community8.jpg',
+        alt: 'Advanced Team Members having a Meeting',
+    }
+]
+
+
 const CommunityBiggestHeartSection: React.FC = () => {
     return (
         <div>
