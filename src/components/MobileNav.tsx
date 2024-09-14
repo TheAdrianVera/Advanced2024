3efd8243fecc19386a5460db931b9dd926f3cbb8
TheAdrianVera <agvm94@gmail.com>
2024-09-14 13:08:10 -0500

    updated mobile nav style

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -29,22 +29,28 @@ const MobileNav: React.FC<MobileNavProps> = ({ className }) => {
                 <div className="fixed inset-0 bg-advancedBlue flex flex-col items-center justify-center z-50">
                     <button className="hover:text-rose-500 absolute top-5 right-5 text-white text-5xl"onClick={toggleMenu}>&times;</button>
                     <ul className="flex flex-col space-y-4 text-white text-2xl">
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <TiHome className="mr-2"/>
                             <Link to="/" onClick={toggleMenu}>Home</Link>
                         </li>
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <PiBooksFill className="mr-2"/>
                             <Link to="/about" onClick={toggleMenu}>About</Link>
                         </li>
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <FaNotesMedical className="mr-2"/>
                             <Link to="/careers" onClick={toggleMenu}>Careers</Link>
                         </li>
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <MdGroups className="mr-2"/>
                             <Link to="/community" onClick={toggleMenu}>Community</Link>
                         </li>
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <FaHandHoldingMedical className="mr-2"/>
                             <Link to="/services" onClick={toggleMenu}>Services</Link>
                         </li>
-                        <li className='hover:text-advancedNavyBlue'>
+                        <li className='hover:text-advancedNavyBlue flex items-center pb-2'>
+                            <MdContactPage className="mr-2"/>
                             <Link to="/contact" onClick={toggleMenu}>Contact</Link>
                         </li>
                     </ul>
