b6cb7e529dbe0e5ffb505a646e8e7f2dd6249cb3
TheAdrianVera <agvm94@gmail.com>
2024-09-14 12:26:20 -0500

    added icon imports for mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 import Logo from './logos/Logo'
 import { TiThMenu } from "react-icons/ti"
 import { Link } from 'react-router-dom'
-
+import { TiHome } from "react-icons/ti"
+import { PiBooksFill } from "react-icons/pi"
+import { FaNotesMedical } from "react-icons/fa"
+import { MdGroups } from "react-icons/md"
+import { FaHandHoldingMedical } from "react-icons/fa"
+import { MdContactPage } from "react-icons/md"
 interface MobileNavProps {
     className?: string
 }
@@ -24,12 +29,24 @@ const MobileNav: React.FC<MobileNavProps> = ({ className }) => {
                 <div className="fixed inset-0 bg-advancedBlue flex flex-col items-center justify-center z-50">
                     <button className="hover:text-rose-500 absolute top-5 right-5 text-white text-5xl"onClick={toggleMenu}>&times;</button>
                     <ul className="flex flex-col space-y-4 text-white text-2xl">
-                        <li className='hover:text-advancedNavyBlue'><Link to="/" onClick={toggleMenu}>Home</Link></li>
-                        <li className='hover:text-advancedNavyBlue'><Link to="/about" onClick={toggleMenu}>About</Link></li>
-                        <li className='hover:text-advancedNavyBlue'><Link to="/careers" onClick={toggleMenu}>Careers</Link></li>
-                        <li className='hover:text-advancedNavyBlue'><Link to="/community" onClick={toggleMenu}>Community</Link></li>
-                        <li className='hover:text-advancedNavyBlue'><Link to="/services" onClick={toggleMenu}>Services</Link></li>
-                        <li className='hover:text-advancedNavyBlue'><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/" onClick={toggleMenu}>Home</Link>
+                        </li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/about" onClick={toggleMenu}>About</Link>
+                        </li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/careers" onClick={toggleMenu}>Careers</Link>
+                        </li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/community" onClick={toggleMenu}>Community</Link>
+                        </li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/services" onClick={toggleMenu}>Services</Link>
+                        </li>
+                        <li className='hover:text-advancedNavyBlue'>
+                            <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+                        </li>
                     </ul>
                 </div>
             )}
