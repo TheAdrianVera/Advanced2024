7886f4d6d34f2e6b87b2e4f7fad65c2698a1e2cb
TheAdrianVera <agvm94@gmail.com>
2024-09-13 19:07:36 -0500

    updated heading of community sections

--- src/components/pagesections/CommunityBiggestHeartSection.tsx
+++ src/components/pagesections/CommunityBiggestHeartSection.tsx
@@ -37,8 +37,13 @@ const biggestHeartAwards = [
 
 const CommunityBiggestHeartSection: React.FC = () => {
     return (
-        <div>
-            BIGGEST HEART
+        <div id='community-biggest-heart' className='section px-5 md:px-20 bg-white pb-20'>
+            <div className='flex flex-col items-center py-10'>
+                <div className='flex flex-col items-center'>
+                    <h1 className='section-title mb-4'>Biggest Heart Award</h1>
+                    <h2 className='text-xl font-arial-narrow md:text-2xl text-gray-800 mb-6'>Help Us Celebrate Biggest Heart Award Winners!</h2>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pagesections/CommunityEventsSection.tsx
+++ src/components/pagesections/CommunityEventsSection.tsx
@@ -19,8 +19,13 @@ const easterEggHuntGallery = [
 
 const CommunityEventsSection:React.FC = () => {
     return (
-        <div>
-            EVENTS SECTION
+        <div id='community-events' className='section px-5 md:px-20 bg-white pb-20'>
+            <div className='flex flex-col items-center py-10'>
+                <div className='flex flex-col items-center'>
+                    <h1 className='section-title mb-4'>Community Events</h1>
+                    <h2 className='text-xl font-arial-narrow md:text-2xl text-gray-800 mb-6'>Learn more about the community events the Advanced Team puts on!</h2>
+                </div>
+            </div>
         </div>
 
     )
